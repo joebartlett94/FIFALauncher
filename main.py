65ed068e7ba721211dad0094be167a91c5f8cf8b
Joe Bartlett <joebartlett94@gmail.com>
2015-10-06 20:34:15 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,69 @@
+"""Runs FIFA's config/launcher, presses enter, then closes the launcher. Run from same dir as FIFA's executable"""
+
+from os import path, listdir
+import re
+import wmi
+import win32gui
+import win32process
+import win32com.client
+
+
+def main():
+    # Init
+    c = wmi.WMI()
+    shell = win32com.client.Dispatch("WScript.Shell")
+
+    # Detecting fifa executable
+    fifa_file = ""
+    found_fifa = False
+    for file in listdir('.'):
+        print file
+        if re.match('fifa[0-9]+.exe', file):
+            found_fifa = True
+            fifa_file = path.join(path.dirname(__file__), file)
+
+    if not found_fifa:
+        print "Unable to find FIFA executable, please run from the same directory as fifa*.exe"
+        return 1
+
+    # Run FIFA config/launcher
+    print("Running FIFA...")
+    shell.run(fifa_file)
+
+    # Get the HWND and PID of FIFA config/launcher
+    config_hwnds = []
+    config_process = None
+    print("Waiting for launcher to load...")
+    while len(config_hwnds) != 1:
+        for process in c.Win32_Process(name="fifaconfig.exe"):
+            config_hwnds = get_hwnds_for_pid(process.ProcessID)
+            config_process = process
+
+    # Give FIFA config/launcher focus
+    win32gui.SetForegroundWindow(config_hwnds[0])
+
+    # Send an 'Enter' keypress
+    shell.SendKeys('{ENTER}', 0)
+
+    # Wait for FIFA proper to load, then kill the config/launcher
+    while True:
+        if len(c.Win32_Process(name=fifa_file)) > 0:
+            config_process.Terminate()
+            return 0
+
+
+# Credit: Tim Golden - http://timgolden.me.uk/python/win32_how_do_i/find-the-window-for-my-subprocess.html
+def get_hwnds_for_pid(pid):
+    def callback(hwnd, hwnds):
+        if win32gui.IsWindowVisible(hwnd) and win32gui.IsWindowEnabled(hwnd):
+            _, found_pid = win32process.GetWindowThreadProcessId(hwnd)
+            if found_pid == pid:
+                hwnds.append(hwnd)
+        return True
+
+    hwnds = []
+    win32gui.EnumWindows(callback, hwnds)
+    return hwnds
+
+if __name__ == "__main__":
+    main()
